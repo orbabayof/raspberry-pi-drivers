1ae57b8beadbac79aee8494c31ffeec397b222af
orbabayof <orbabayof@gmail.com>
2025-10-04 13:23:01 +0300

    first kernel mod

---
+++ hello_world/hello.c
@@ -0,0 +1,25 @@
+#include "linux/module.h"
+#include "linux/init.h"
+
+
+int my_init(void);
+void my_exit(void);
+
+int my_init(void)
+{
+  printk("init\n");
+  return 0;
+}
+
+void my_exit(void)
+{
+  printk("exit\n");
+}
+
+module_init(my_init);
+module_exit(my_exit);
+
+MODULE_LICENSE("GPL");
+
+
+
